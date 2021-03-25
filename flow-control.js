12e2626474a29d2c545f4941917ce193e5c6c289
Jessica Stayton <jessica.stayton@gmail.com>
2021-03-25 17:14:17 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,7 +29,6 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   case (age >= 13 && age <= 19) :
     return ("You are a teenager!");
-      break;
   default:
      return ("You have an age");
 }
