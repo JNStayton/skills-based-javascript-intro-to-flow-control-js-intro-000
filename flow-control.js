7b59c38674cbb8efc1f8a7fc6d0c9100723c13c9
Jessica Stayton <jessica.stayton@gmail.com>
2021-03-25 16:44:55 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -22,8 +22,8 @@ function ageChecker(age) {
 
 function ternaryTeenager(age) {
   return age <= 13 && >= 19 ?
-  "You are a teenager" :
-  "You are not a teenager"
+  You are a teenager :
+  You are not a teenager
 }
 
 function switchAge(age) {
